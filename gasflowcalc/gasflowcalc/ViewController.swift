9ab55af558307d7a58bb7d2c8f4cb00b6cb3532f
Code-Critter <barkeng@gmail.com>
2019-02-23 22:26:34 -0600

    initial creation
    initial creation

---
+++ gasflowcalc/gasflowcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gasflowcalc
+//
+//  Created by W N Barker on 2/23/19.
+//  Copyright © 2019 W N Barker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
